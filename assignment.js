ed3805d32c668012f38341e9c55a4dc47f9d113d
Stephen Boahene <stephenboahenesenior@gmail.com>
2024-10-22 19:39:46 +0000

    javaScript
    basic knowledge on javaScript

---
+++ assignment.js
@@ -0,0 +1,24 @@
+// initialize a counter variable. let counter = 0;
+let count = 0;
+
+// function to increment the count by 1
+function increment () {
+  count += 1;
+  console.log(count);
+}
+increment()
+
+// funtion to decrement the count by 1
+function decrement() {
+  count -= 1;
+  console.log(count);
+
+}
+decrement()
+
+// function to display the current count in the console
+function displayCount() {
+  console.log(count);
+
+}
+displayCount()
